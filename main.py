0968e3d4b633bcedadfc1d3ce61b6f5e65f96a3d
mahmoud elhissy <mahmoudelhissy@std.mansnu.edu.eg>
2026-05-13 01:03:09 +0300

    GUI design created

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
- Appointments Section
-# =========================
-# MEMBER 3 - APPOINTMENTS
-# =========================
 
 appointments = []
 
@@ -35,5 +31,58 @@ def show_appointments():
         text = "No appointments"
 
     messagebox.showinfo("Appointments", text)
-Commit
-git commit -m "Appointments system added"
+window = tk.Tk()
+
+window.title("Hospital Management System")
+
+window.geometry("450x600")
+
+window.configure(bg="lightblue")
+
+title = tk.Label(
+    window,
+    text="Hospital System",
+    font=("Arial", 18, "bold"),
+    bg="lightblue"
+)
+
+title.pack(pady=10)
+
+# Patients UI
+tk.Label(window, text="Patient Name", bg="lightblue").pack()
+
+entry_patient = tk.Entry(window)
+entry_patient.pack()
+
+tk.Button(window, text="Add Patient", command=add_patient).pack()
+
+tk.Button(window, text="Delete Patient", command=delete_patient).pack()
+
+tk.Button(window, text="Search Patient", command=search_patient).pack()
+
+
+# Doctors UI
+tk.Label(window, text="\nDoctor Name", bg="lightblue").pack()
+
+entry_doctor = tk.Entry(window)
+entry_doctor.pack()
+
+tk.Button(window, text="Add Doctor", command=add_doctor).pack()
+
+tk.Button(window, text="Search Doctor", command=search_doctor).pack()
+
+
+# Appointment UI
+tk.Label(window, text="\nAppointment Patient", bg="lightblue").pack()
+
+entry_app_patient = tk.Entry(window)
+entry_app_patient.pack()
+
+tk.Label(window, text="Appointment Doctor", bg="lightblue").pack()
+
+entry_app_doctor = tk.Entry(window)
+entry_app_doctor.pack()
+
+tk.Button(window, text="Book Appointment", command=book_appointment).pack()
+
+tk.Button(window, text="Show Appointments", command=show_appointments).pack()
